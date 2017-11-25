613d2fc8dc5187a76d1ac66e93ff09a43f27a23f
rahul <rahul@rahulkant.com>
2017-11-25 16:56:18 +0530

    searchbox with style added

--- src/routes/Home/components/SearchBox/index.js
+++ src/routes/Home/components/SearchBox/index.js
@@ -2,16 +2,19 @@ import React from 'react';
 import {Text} from 'react-native';
 import {View, InputGroup, Input } from 'native-base';
 import styles from './SearchBoxStyles';
+import Icon from "react-native-vector-icons/FontAwesome"
 
 export const SearchBox = () => {
 
     return (
       <View style={styles.searchBox}>
-      	<Text style={styles.label}> PICK UP </Text>
-      	<InputGroup>
-      		<Input style={styles.inputSearch} placeholder= "choose pick-up location"/>
-      	</InputGroup>
-
+	      <View style={styles.inputWrapper}>
+	      	<Text style={styles.label}> PICK UP </Text>
+	      	<InputGroup>
+	      		<Icon name="search" size={15} color="orange"/>
+	      		<Input style={styles.inputSearch} placeholder= "choose pick-up location"/>
+	      	</InputGroup>
+	      </View>
       </View>
     );
 };
