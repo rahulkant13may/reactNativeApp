5ac585a027e01160a43ca52672f3f3a41a2c567b
rahul <rahul@rahulkant.com>
2017-11-25 16:49:13 +0530

    ejected from expo

---
+++ android/app/src/main/java/org/test/generated/ExponentBuildConstants.java
@@ -0,0 +1,8 @@
+package org.test.generated;
+
+// This file is auto-generated. Please don't rename!
+public class ExponentBuildConstants {
+
+  public static final String DEVELOPMENT_URL = "expa84d92258b6c483d8cbbb48f9950f60a://sa-3rx.rahulkant13may.expotaxiapp.exp.direct:80";
+
+}
